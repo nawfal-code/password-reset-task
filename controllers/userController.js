1e6c3b978cbb30f13ccca494c55e2cda4f0102b2
Mohammed Nawfal <nawfal.mohammed006@gmail.com>
2026-01-09 17:05:27 +0530

    Refactor userController with consistent imports

--- controllers/userController.js
+++ controllers/userController.js
@@ -89,16 +89,17 @@ export const forgotPassword = async (req, res) => {
     // const token = jwt.sign(
     //   { _id: user._id },
     //   process.env.SECRET_KEY,
-    //   { expiresIn: "30m" }
+    //   { expiresIn: "1h" }
     // );
 
     // const resetLink = `http://localhost:5173/password-reset/${token}`;
 
-    // await sendEmail(
-    //   normalizedEmail,
-    //   "Password Reset Link",
-    //   `Click the link to reset your password: ${resetLink}`
-    // );
+    await sendEmail(
+      normalizedEmail,
+      "Password Reset Link",
+     "check check"
+      // `Click the link to reset your password: ${resetLink}`
+    );
 
     return res.status(200).json({
       message: "a reset link sent to email succesfully",
@@ -138,6 +139,7 @@ export const resetPassword = async (req, res) => {
     return res.status(500).json({ message: error.message });
   }
 };
+
 
 
 
