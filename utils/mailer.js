d69c0828c36fe1007b9bc66c60fb8ba69e80a8f5
Mohammed Nawfal <nawfal.mohammed006@gmail.com>
2026-01-10 16:02:21 +0530

    Refactor mailer to use SMTP with secure settings
    Updated mailer configuration to use SMTP settings with a secure connection and increased connection timeout.

--- utils/mailer.js
+++ utils/mailer.js
@@ -4,12 +4,16 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true, // true for 465, false for 587
   auth: {
     user: process.env.PASS_MAIL,
-    pass: process.env.PASS_KEY,
+    pass: process.env.PASS_KEY, 
   },
+  connectionTimeout: 20000, // 20s
 });
+
 // send mail
 const sendEmail = async (to, subject, text) => {
   try {
@@ -29,3 +33,4 @@ const sendEmail = async (to, subject, text) => {
 };
 
 export default sendEmail;
+
