32f492c715e613cb1690cf9c6695041c2c7aabb0
Mohammed Nawfal <nawfal.mohammed006@gmail.com>
2026-01-10 15:46:08 +0530

    Refactor userController to improve functionality

--- controllers/userController.js
+++ controllers/userController.js
@@ -94,15 +94,14 @@ export const forgotPassword = async (req, res) => {
 
     const resetLink = `http://localhost:5173/password-reset/${token}`;
     
-    // await sendEmail(
-    //    email,
-    //   "Password Reset Link",
-    // `Click the link to reset your password: ${resetLink} 
-    // );
+    await sendEmail(
+       email,
+      "Password Reset Link",
+    `Click the link to reset your password: ${resetLink} 
+    );
  
-// `Click the link to reset your password: ${resetLink}`
     return res.status(200).json({
-      message: `send by ${email}`
+      message: `email sent succesfully`
     });
 
   } catch (error) {
@@ -139,6 +138,7 @@ export const resetPassword = async (req, res) => {
     return res.status(500).json({ message: error.message });
   }
 };
+
 
 
 
